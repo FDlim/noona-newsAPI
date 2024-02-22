219fc97aef5e527cf564d9b31550d93540a7d74e
FDlim <lyongl4208@gmail.com>
2024-02-22 20:39:28 +0900

    모바일 최적화

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const API_KEY = "3168ee9b3c9d45d6b57b5fb04d1b6f37";
 let newsList = [];
 const menus = document.querySelectorAll(".menus button");
+const sideMenus = document.querySelectorAll(".side-menus button");
 menus.forEach((menus) =>
+  menus.addEventListener("click", (event) => getNewsByCategory(event))
+);
+sideMenus.forEach((menus) =>
   menus.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
@@ -66,3 +70,52 @@ const render = () => {
 };
 
 getLatestNews();
+let searchToggleSwitch = false;
+const searchToggle = document.getElementById("search-input-toggle");
+searchToggle.addEventListener("click", () => {
+  let input = document.querySelector(".input-toggle");
+  if (searchToggleSwitch == false) {
+    input.style.display = "block";
+    searchToggleSwitch = true;
+  } else {
+    input.style.display = "none";
+    searchToggleSwitch = false;
+  }
+  console.log(input);
+});
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  document.querySelector(
+    ".side-menus-open"
+  ).innerHTML = `<section class="menu-container">
+  <div class="exit-button">
+    <svg
+      class="exit-button-click"
+      cursor="pointer"
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      fill="currentColor"
+      class="bi bi-x-lg"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"
+      />
+    </svg>
+  </div>
+  <div class="side-menus">
+    <button>Business</button>
+    <button>Entertainment</button>
+    <button>General</button>
+    <button>Health</button>
+    <button>Science</button>
+    <button>Sports</button>
+    <button>Technology</button>
+  </div>
+</section>`;
+  const exitButton = document.querySelector(".exit-button-click");
+  exitButton.addEventListener("click", () => {
+    document.querySelector(".menu-container").style.display = "none";
+  });
+});
